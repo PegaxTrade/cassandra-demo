16a40d447262e229a27d7149daebd2e3750ca179
unyoungwax <yuxuan@mtrlabs.com>
2024-12-06 10:58:29 +0800

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,13 @@ import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import com.datastax.oss.driver.api.core.metadata.schema.ClusteringOrder;
 import com.datastax.oss.driver.api.core.type.DataTypes;
 import com.datastax.oss.driver.shaded.guava.common.collect.ImmutableMap;
+import org.apache.commons.collections4.ListUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
@@ -33,8 +35,10 @@ public class Main {
         final String keyspace = "demo";
         final int replicationFactor = 3;
         final String table = "lorem";
+
         final int itemCount = 50000;
-        final int threadCount = 100;
+        final int writeThreadCount = 50;
+        final int readThreadCount = 500;
 
         try (final CassandraClient client = new CassandraClient(datacenter, ips, port)) {
             Main.log("createKeyspaceIfNotExists");
@@ -51,63 +55,23 @@ public class Main {
 
             Main.log(String.format("Number of processors: %d", Runtime.getRuntime().availableProcessors()));
 
-            final List<List<Integer>> batches = Main.createBatches(itemCount, threadCount);
+            final List<Integer> values = Main.createValues(itemCount);
+            final List<List<Integer>> writeBatches = partitionList(values, writeThreadCount);
+            final List<List<Integer>> readBatches = partitionList(values, readThreadCount);
 
-            Main.log(String.format("Inserting %d items using %d threads", itemCount, threadCount));
+            Main.evaluateInsert(
+                    client,
+                    keyspace,
+                    table,
+                    writeBatches
+            );
 
-            final Duration durationInsert = Main.stopwatch(() -> {
-                final List<Thread> threads = batches.stream()
-                        .map((batch) -> {
-                            return new Thread(() -> {
-                                for (int value : batch) {
-                                    Main.insertRow(client, keyspace, table, value);
-                                }
-                            });
-                        }).toList();
-
-                Main.runInThreads(threads);
-            });
-
-            Main.log(String.format(
-                    "Inserting %d items using %d threads takes %.2f seconds (average: %.2f per second)",
-                    itemCount,
-                    threadCount,
-                    (double) durationInsert.toMillis() / 1000,
-                    (double) itemCount / durationInsert.toMillis() * 1000
-            ));
-
-            Main.log(String.format("Selecting %d items randomly using %d threads", itemCount, threadCount));
-
-            final Duration durationSelect = Main.stopwatch(() -> {
-                final List<Thread> threads = batches.stream()
-                        .map((batch) -> {
-                            return new Thread(() -> {
-                                final Random random = new Random();
-
-                                for (int i = 0; i < batch.size(); i++) {
-                                    final int index = random.nextInt(0, batch.size());
-                                    final int value = batch.get(index);
-
-                                    final Row row = Main.selectRow(client, keyspace, table, value).one();
-
-                                    if (row == null) {
-                                        Main.log(String.format("Missing value %d", value));
-                                    }
-                                }
-                            });
-                        })
-                        .toList();
-
-                Main.runInThreads(threads);
-            });
-
-            Main.log(String.format(
-                    "Selecting %d items randomly using %d threads takes %.2f seconds (average: %.2f per second)",
-                    itemCount,
-                    threadCount,
-                    (double) durationSelect.toMillis() / 1000,
-                    (double) itemCount / durationSelect.toMillis() * 1000
-            ));
+            Main.evaluateSelect(
+                    client,
+                    keyspace,
+                    table,
+                    readBatches
+            );
         }
     }
 
@@ -115,17 +79,95 @@ public class Main {
         System.out.format("[%s] %s%n", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), message);
     }
 
-    private static List<List<Integer>> createBatches(int rowCount, int threadCount) {
+    private static void evaluateInsert(
+            @NotNull CassandraClient client,
+            @NotNull String keyspace,
+            @NotNull String table,
+            @NotNull List<List<Integer>> batches
+    ) {
+        final int threadCount = batches.size();
+        final int itemCount = batches.stream().mapToInt(Collection::size).sum();
+
+        Main.log(String.format("Inserting %d items using %d threads", itemCount, threadCount));
+
+        final Duration durationInsert = Main.stopwatch(() -> {
+            final List<Thread> threads = batches.stream()
+                    .map((batch) -> {
+                        return new Thread(() -> {
+                            for (int value : batch) {
+                                Main.insertRow(client, keyspace, table, value);
+                            }
+                        });
+                    }).toList();
+
+            Main.runInThreads(threads);
+        });
+
+        Main.log(String.format(
+                "Inserting %d items using %d threads takes %.2f seconds (average: %.2f per second)",
+                itemCount,
+                threadCount,
+                (double) durationInsert.toMillis() / 1000,
+                (double) itemCount / durationInsert.toMillis() * 1000
+        ));
+    }
+
+    private static void evaluateSelect(
+            @NotNull CassandraClient client,
+            @NotNull String keyspace,
+            @NotNull String table,
+            @NotNull List<List<Integer>> batches
+    ) {
+        final int threadCount = batches.size();
+        final int itemCount = batches.stream().mapToInt(Collection::size).sum();
+
+        Main.log(String.format("Selecting %d items randomly using %d threads", itemCount, threadCount));
+
+        final Duration durationSelect = Main.stopwatch(() -> {
+            final List<Thread> threads = batches.stream()
+                    .map((batch) -> {
+                        return new Thread(() -> {
+                            final Random random = new Random();
+
+                            for (int i = 0; i < batch.size(); i++) {
+                                final int index = random.nextInt(0, batch.size());
+                                final int value = batch.get(index);
+
+                                final Row row = Main.selectRow(client, keyspace, table, value).one();
+
+                                if (row == null) {
+                                    Main.log(String.format("Missing value %d", value));
+                                }
+                            }
+                        });
+                    })
+                    .toList();
+
+            Main.runInThreads(threads);
+        });
+
+        Main.log(String.format(
+                "Selecting %d items randomly using %d threads takes %.2f seconds (average: %.2f per second)",
+                itemCount,
+                threadCount,
+                (double) durationSelect.toMillis() / 1000,
+                (double) itemCount / durationSelect.toMillis() * 1000
+        ));
+    }
+
+    private static List<Integer> createValues(int count) {
         final Random random = new Random();
 
-        return IntStream.range(0, threadCount)
-                .mapToObj((threadIndex) -> {
-                    return IntStream.generate(random::nextInt)
-                            .limit(rowCount / threadCount)
-                            .boxed()
-                            .toList();
-                })
+        return IntStream.generate(random::nextInt)
+                .limit(count)
+                .boxed()
                 .toList();
+    }
+
+    private static <T> @NotNull List<List<T>> partitionList(@NotNull List<T> list, int listCount) {
+        final int size = (int) Math.ceil((double) list.size() / listCount);
+
+        return ListUtils.partition(list, size);
     }
 
     private static void runInThreads(@NotNull List<Thread> threads) {
