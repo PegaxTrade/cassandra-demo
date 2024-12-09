349a9020cb5a0fe139c38f44a38dae1cd7bd0519
unyoungwax <yuxuan@mtrlabs.com>
2024-12-09 17:47:52 +0800

    update

--- src/main/java/org/example/CassandraClient.java
+++ src/main/java/org/example/CassandraClient.java
@@ -1,65 +1,64 @@
 package org.example;
 
+import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createKeyspace;
+import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.dropTable;
+
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
-import org.jetbrains.annotations.NotNull;
-
 import java.net.InetSocketAddress;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-
-import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createKeyspace;
-import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.dropTable;
+import org.jetbrains.annotations.NotNull;
 
 public final class CassandraClient implements AutoCloseable {
-    private final CqlSession session;
+  private final CqlSession session;
 
-    public CassandraClient(
-            final String datacenter,
-            final String[] ips,
-            final int port
-    ) {
-        final DriverConfigLoader loader = DriverConfigLoader.programmaticBuilder()
-                .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(60))
-                .build();
+  public CassandraClient(
+      final String datacenter,
+      final String[] ips,
+      final int port
+  ) {
+    final DriverConfigLoader loader = DriverConfigLoader.programmaticBuilder()
+        .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(60))
+        .build();
 
-        final List<InetSocketAddress> contactPoints = Arrays.stream(ips)
-                .map((ip) -> new InetSocketAddress(ip, port))
-                .toList();
+    final List<InetSocketAddress> contactPoints = Arrays.stream(ips)
+        .map((ip) -> new InetSocketAddress(ip, port))
+        .toList();
 
-        this.session = CqlSession.builder()
-                .withConfigLoader(loader)
-                .addContactPoints(contactPoints)
-                .withLocalDatacenter(datacenter)
-                .build();
-    }
+    this.session = CqlSession.builder()
+        .withConfigLoader(loader)
+        .addContactPoints(contactPoints)
+        .withLocalDatacenter(datacenter)
+        .build();
+  }
 
-    public void close() {
-        this.session.close();
-    }
+  public void close() {
+    this.session.close();
+  }
 
-    public @NotNull ResultSet execute(final SimpleStatement statement) {
-        return this.session.execute(statement);
-    }
+  public @NotNull ResultSet execute(final SimpleStatement statement) {
+    return this.session.execute(statement);
+  }
 
-    public void createKeyspaceIfNotExists(final String keyspace, final int replicationFactor) {
-        final SimpleStatement statement = createKeyspace(keyspace)
-                .ifNotExists()
-                .withSimpleStrategy(replicationFactor)
-                .build();
+  public void createKeyspaceIfNotExists(final String keyspace, final int replicationFactor) {
+    final SimpleStatement statement = createKeyspace(keyspace)
+        .ifNotExists()
+        .withSimpleStrategy(replicationFactor)
+        .build();
 
-        this.execute(statement);
-    }
+    this.execute(statement);
+  }
 
-    public void dropTableIfExists(final String keyspace, final String table) {
-        final SimpleStatement statement = dropTable(keyspace, table)
-                .ifExists()
-                .build();
+  public void dropTableIfExists(final String keyspace, final String table) {
+    final SimpleStatement statement = dropTable(keyspace, table)
+        .ifExists()
+        .build();
 
-        this.execute(statement);
-    }
+    this.execute(statement);
+  }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,17 @@
 package org.example;
 
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.insertInto;
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createIndex;
+import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createTable;
+
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import com.datastax.oss.driver.api.core.metadata.schema.ClusteringOrder;
 import com.datastax.oss.driver.api.core.type.DataTypes;
 import com.datastax.oss.driver.shaded.guava.common.collect.ImmutableMap;
-import org.apache.commons.collections4.ListUtils;
-import org.jetbrains.annotations.NotNull;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -16,253 +19,256 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
-
-import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.insertInto;
-import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
-import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
-import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createIndex;
-import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createTable;
+import org.apache.commons.collections4.ListUtils;
+import org.jetbrains.annotations.NotNull;
 
 public final class Main {
-    public static final int MILLISECONDS_PER_SECOND = 1000;
+  public static final int MILLISECONDS_PER_SECOND = 1000;
 
-    private Main() {
-        // Utility classes should not have a public or default constructor.
+  private Main() {
+    // Utility classes should not have a public or default constructor.
+  }
+
+  public static void main(final String[] args) {
+    final String datacenter = "datacenter1";
+    final String[] ips = {
+        "192.168.0.163",
+        "192.168.0.24",
+        "192.168.0.214"
+    };
+    final int port = 9042;
+    final String keyspace = "demo";
+    final int replicationFactor = 3;
+    final String table = "lorem";
+
+    final int itemCount = 50000;
+    final int writeThreadCount = 50;
+    final int readThreadCount = 500;
+
+    try (CassandraClient client = new CassandraClient(datacenter, ips, port)) {
+      Main.log("createKeyspaceIfNotExists");
+
+      client.createKeyspaceIfNotExists(keyspace, replicationFactor);
+
+      Main.log("tableCreate");
+
+      Main.tableCreate(client, keyspace, table);
+
+      Main.log("indexCreate");
+
+      Main.indexCreate(client, keyspace, table);
+
+      final int processorCount = Runtime.getRuntime().availableProcessors();
+
+      Main.log(String.format("Number of processors: %d", processorCount));
+
+      final List<Integer> values = Main.createValues(itemCount);
+      final List<List<Integer>> writeBatches = partitionList(values, writeThreadCount);
+      final List<List<Integer>> readBatches = partitionList(values, readThreadCount);
+
+      Main.evaluateInsert(
+          client,
+          keyspace,
+          table,
+          writeBatches
+      );
+
+      Main.evaluateSelect(
+          client,
+          keyspace,
+          table,
+          readBatches
+      );
+    }
+  }
+
+  private static void log(@NotNull final String message) {
+    final String time = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+
+    System.out.format("[%s] %s%n", time, message);
+  }
+
+  private static void evaluateInsert(
+      @NotNull final CassandraClient client,
+      @NotNull final String keyspace,
+      @NotNull final String table,
+      @NotNull final List<List<Integer>> batches
+  ) {
+    final int threadCount = batches.size();
+    final int itemCount = batches.stream().mapToInt(Collection::size).sum();
+
+    Main.log(String.format("Inserting %d items using %d threads", itemCount, threadCount));
+
+    final Duration durationInsert = Main.stopwatch(() -> {
+      final List<Thread> threads = batches.stream()
+          .map((batch) -> {
+            return new Thread(() -> {
+              for (int value : batch) {
+                Main.insertRow(client, keyspace, table, value);
+              }
+            });
+          }).toList();
+
+      Main.runThreads(threads);
+    });
+
+    Main.log(String.format(
+        "Inserting %d items using %d threads takes %.2f seconds (average: %.2f per second)",
+        itemCount,
+        threadCount,
+        (double) durationInsert.toMillis() / MILLISECONDS_PER_SECOND,
+        (double) itemCount / durationInsert.toMillis() * MILLISECONDS_PER_SECOND
+    ));
+  }
+
+  private static void evaluateSelect(
+      @NotNull final CassandraClient client,
+      @NotNull final String keyspace,
+      @NotNull final String table,
+      @NotNull final List<List<Integer>> batches
+  ) {
+    final int threadCount = batches.size();
+    final int itemCount = batches.stream().mapToInt(Collection::size).sum();
+
+    Main.log(String.format("Selecting %d items randomly using %d threads", itemCount, threadCount));
+
+    final Duration durationSelect = Main.stopwatch(() -> {
+      final List<Thread> threads = batches.stream()
+          .map((batch) -> {
+            return new Thread(() -> {
+              final Random random = new Random();
+
+              for (int i = 0; i < batch.size(); i++) {
+                final int index = random.nextInt(0, batch.size());
+                final int value = batch.get(index);
+
+                final Row row = Main.selectRow(client, keyspace, table, value).one();
+
+                if (row == null) {
+                  Main.log(String.format("Missing value %d", value));
+                }
+              }
+            });
+          })
+          .toList();
+
+      Main.runThreads(threads);
+    });
+
+    Main.log(String.format(
+        "Selecting %d items randomly using %d threads takes %.2f seconds (average: %.2f per second)",
+        itemCount,
+        threadCount,
+        (double) durationSelect.toMillis() / MILLISECONDS_PER_SECOND,
+        (double) itemCount / durationSelect.toMillis() * MILLISECONDS_PER_SECOND
+    ));
+  }
+
+  private static List<Integer> createValues(final int count) {
+    final Random random = new Random();
+
+    return IntStream.generate(random::nextInt)
+        .limit(count)
+        .boxed()
+        .toList();
+  }
+
+  private static <T> @NotNull List<List<T>> partitionList(
+      @NotNull final List<T> list,
+      final int listCount
+  ) {
+    final int size = (int) Math.ceil((double) list.size() / listCount);
+
+    return ListUtils.partition(list, size);
+  }
+
+  private static void runThreads(@NotNull final List<Thread> threads) {
+    for (Thread thread : threads) {
+      thread.start();
     }
 
-    public static void main(final String[] args) {
-        final String datacenter = "datacenter1";
-        final String[] ips = {
-                "192.168.0.163",
-                "192.168.0.24",
-                "192.168.0.214"
-        };
-        final int port = 9042;
-        final String keyspace = "demo";
-        final int replicationFactor = 3;
-        final String table = "lorem";
-
-        final int itemCount = 50000;
-        final int writeThreadCount = 50;
-        final int readThreadCount = 500;
-
-        try (CassandraClient client = new CassandraClient(datacenter, ips, port)) {
-            Main.log("createKeyspaceIfNotExists");
-
-            client.createKeyspaceIfNotExists(keyspace, replicationFactor);
-
-            Main.log("tableCreate");
-
-            Main.tableCreate(client, keyspace, table);
-
-            Main.log("indexCreate");
-
-            Main.indexCreate(client, keyspace, table);
-
-            Main.log(String.format("Number of processors: %d", Runtime.getRuntime().availableProcessors()));
-
-            final List<Integer> values = Main.createValues(itemCount);
-            final List<List<Integer>> writeBatches = partitionList(values, writeThreadCount);
-            final List<List<Integer>> readBatches = partitionList(values, readThreadCount);
-
-            Main.evaluateInsert(
-                    client,
-                    keyspace,
-                    table,
-                    writeBatches
-            );
-
-            Main.evaluateSelect(
-                    client,
-                    keyspace,
-                    table,
-                    readBatches
-            );
-        }
+    for (Thread thread : threads) {
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        Main.log("A thread was interrupted.");
+      }
     }
+  }
 
-    private static void log(@NotNull final String message) {
-        System.out.format("[%s] %s%n", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), message);
-    }
+  private static Duration stopwatch(@NotNull final Runnable callback) {
+    final long timeStart = System.currentTimeMillis();
 
-    private static void evaluateInsert(
-            @NotNull final CassandraClient client,
-            @NotNull final String keyspace,
-            @NotNull final String table,
-            @NotNull final List<List<Integer>> batches
-    ) {
-        final int threadCount = batches.size();
-        final int itemCount = batches.stream().mapToInt(Collection::size).sum();
+    callback.run();
 
-        Main.log(String.format("Inserting %d items using %d threads", itemCount, threadCount));
+    final long timeEnd = System.currentTimeMillis();
 
-        final Duration durationInsert = Main.stopwatch(() -> {
-            final List<Thread> threads = batches.stream()
-                    .map((batch) -> {
-                        return new Thread(() -> {
-                            for (int value : batch) {
-                                Main.insertRow(client, keyspace, table, value);
-                            }
-                        });
-                    }).toList();
+    return Duration.ofMillis(timeEnd - timeStart);
+  }
 
-            Main.runThreads(threads);
-        });
+  private static void tableCreate(
+      @NotNull final CassandraClient client,
+      @NotNull final String keyspace,
+      @NotNull final String table
+  ) {
+    final SimpleStatement statement = createTable(keyspace, table)
+        .ifNotExists()
+        .withPartitionKey("id", DataTypes.TEXT)
+        .withClusteringColumn("value", DataTypes.INT)
+        .withColumn("hex", DataTypes.TEXT)
+        .withClusteringOrder("value", ClusteringOrder.DESC)
+        .build();
 
-        Main.log(String.format(
-                "Inserting %d items using %d threads takes %.2f seconds (average: %.2f per second)",
-                itemCount,
-                threadCount,
-                (double) durationInsert.toMillis() / MILLISECONDS_PER_SECOND,
-                (double) itemCount / durationInsert.toMillis() * MILLISECONDS_PER_SECOND
-        ));
-    }
+    client.execute(statement);
+  }
 
-    private static void evaluateSelect(
-            @NotNull final CassandraClient client,
-            @NotNull final String keyspace,
-            @NotNull final String table,
-            @NotNull final List<List<Integer>> batches
-    ) {
-        final int threadCount = batches.size();
-        final int itemCount = batches.stream().mapToInt(Collection::size).sum();
+  private static void indexCreate(
+      @NotNull final CassandraClient client,
+      @NotNull final String keyspace,
+      @NotNull final String table
+  ) {
+    final SimpleStatement statement = createIndex()
+        .ifNotExists()
+        .usingSASI()
+        .onTable(keyspace, table)
+        .andColumn("hex")
+        .withSASIOptions(ImmutableMap.of("mode", "CONTAINS"))
+        .build();
 
-        Main.log(String.format("Selecting %d items randomly using %d threads", itemCount, threadCount));
+    client.execute(statement);
+  }
 
-        final Duration durationSelect = Main.stopwatch(() -> {
-            final List<Thread> threads = batches.stream()
-                    .map((batch) -> {
-                        return new Thread(() -> {
-                            final Random random = new Random();
+  private static void insertRow(
+      @NotNull final CassandraClient client,
+      @NotNull final String keyspace,
+      @NotNull final String table,
+      final int value
+  ) {
+    final String id = Integer.toString(value);
+    final String hex = Integer.toHexString(value);
 
-                            for (int i = 0; i < batch.size(); i++) {
-                                final int index = random.nextInt(0, batch.size());
-                                final int value = batch.get(index);
+    final SimpleStatement statement = insertInto(keyspace, table)
+        .value("id", literal(id))
+        .value("value", literal(value))
+        .value("hex", literal(hex))
+        .build();
 
-                                final Row row = Main.selectRow(client, keyspace, table, value).one();
+    client.execute(statement);
+  }
 
-                                if (row == null) {
-                                    Main.log(String.format("Missing value %d", value));
-                                }
-                            }
-                        });
-                    })
-                    .toList();
+  private static @NotNull ResultSet selectRow(
+      @NotNull final CassandraClient client,
+      @NotNull final String keyspace,
+      @NotNull final String table,
+      final int value
+  ) {
+    final String id = Integer.toString(value);
 
-            Main.runThreads(threads);
-        });
+    final SimpleStatement statement = selectFrom(keyspace, table)
+        .all()
+        .whereColumn("id").isEqualTo(literal(id))
+        .build();
 
-        Main.log(String.format(
-                "Selecting %d items randomly using %d threads takes %.2f seconds (average: %.2f per second)",
-                itemCount,
-                threadCount,
-                (double) durationSelect.toMillis() / MILLISECONDS_PER_SECOND,
-                (double) itemCount / durationSelect.toMillis() * MILLISECONDS_PER_SECOND
-        ));
-    }
-
-    private static List<Integer> createValues(final int count) {
-        final Random random = new Random();
-
-        return IntStream.generate(random::nextInt)
-                .limit(count)
-                .boxed()
-                .toList();
-    }
-
-    private static <T> @NotNull List<List<T>> partitionList(@NotNull final List<T> list, final int listCount) {
-        final int size = (int) Math.ceil((double) list.size() / listCount);
-
-        return ListUtils.partition(list, size);
-    }
-
-    private static void runThreads(@NotNull final List<Thread> threads) {
-        for (Thread thread : threads) {
-            thread.start();
-        }
-
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                Main.log("A thread was interrupted.");
-            }
-        }
-    }
-
-    private static Duration stopwatch(@NotNull final Runnable callback) {
-        final long timeStart = System.currentTimeMillis();
-
-        callback.run();
-
-        final long timeEnd = System.currentTimeMillis();
-
-        return Duration.ofMillis(timeEnd - timeStart);
-    }
-
-    private static void tableCreate(
-            @NotNull final CassandraClient client,
-            @NotNull final String keyspace,
-            @NotNull final String table
-    ) {
-        final SimpleStatement statement = createTable(keyspace, table)
-                .ifNotExists()
-                .withPartitionKey("id", DataTypes.TEXT)
-                .withClusteringColumn("value", DataTypes.INT)
-                .withColumn("hex", DataTypes.TEXT)
-                .withClusteringOrder("value", ClusteringOrder.DESC)
-                .build();
-
-        client.execute(statement);
-    }
-
-    private static void indexCreate(
-            @NotNull final CassandraClient client,
-            @NotNull final String keyspace,
-            @NotNull final String table
-    ) {
-        final SimpleStatement statement = createIndex()
-                .ifNotExists()
-                .usingSASI()
-                .onTable(keyspace, table)
-                .andColumn("hex")
-                .withSASIOptions(ImmutableMap.of("mode", "CONTAINS"))
-                .build();
-
-        client.execute(statement);
-    }
-
-    private static void insertRow(
-            @NotNull final CassandraClient client,
-            @NotNull final String keyspace,
-            @NotNull final String table,
-            final int value
-    ) {
-        final String id = Integer.toString(value);
-        final String hex = Integer.toHexString(value);
-
-        final SimpleStatement statement = insertInto(keyspace, table)
-                .value("id", literal(id))
-                .value("value", literal(value))
-                .value("hex", literal(hex))
-                .build();
-
-        client.execute(statement);
-    }
-
-    private static @NotNull ResultSet selectRow(
-            @NotNull final CassandraClient client,
-            @NotNull final String keyspace,
-            @NotNull final String table,
-            final int value
-    ) {
-        final String id = Integer.toString(value);
-
-        final SimpleStatement statement = selectFrom(keyspace, table)
-                .all()
-                .whereColumn("id").isEqualTo(literal(id))
-                .build();
-
-        return client.execute(statement);
-    }
+    return client.execute(statement);
+  }
 }
